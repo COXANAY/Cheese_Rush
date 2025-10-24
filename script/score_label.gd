3ebf0b390186ef69943f236dcb8b23d7ebd877a2
GUILHERME WOREL <guilherme.worel@catolicasc.edu.br>
2025-10-23 21:54:17 -0300

    sistema de pontuação acre lixo

--- script/score_label.gd
+++ script/score_label.gd
@@ -2,7 +2,33 @@ extends Label
 
 var score = 0
 
+@onready var database = SQLite.new()
+@onready var NetworkManager = get_node("/root/NetworkManager")
+
+func _ready():
+	# Abre o banco de dados
+	database.path = "res://data.db"
+	database.open_db()
+	
+	# Carrega a pontuação do usuário logado, se houver
+	var username = NetworkManager.logged_in_username
+	if username != "Convidado":
+		var result = database.select_rows("players", "username = '" + username + "'", ["pontuacao"])
+		if result.size() > 0:
+			score = result[0]["pontuacao"]
+			text = "Pontuação: " + str(score)
+		else:
+			text = "Pontuação: 0"
+	else:
+		text = "Pontuação: 0"
+
 func _on_collectible_collected(points = 1):
-	# O valor padrão 'points = 1' garante que, se for chamado sem argumento, some 1 ponto.
+	# Incrementa a pontuação local
 	score += points
 	text = "Pontuação: " + str(score)
+	
+	# Salva no banco se houver usuário logado
+	var username = NetworkManager.logged_in_username
+	if username != "Convidado":
+		var update_query = "UPDATE players SET pontuacao = " + str(score) + " WHERE username = '" + username + "';"
+		database.query(update_query)
